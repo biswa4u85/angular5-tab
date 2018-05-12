7e735e367396a62ccd8c475342769d70e343918f
Rakesh Biswal <rakesh@techlift.in>
2018-05-12 17:37:33 +0530

    HTML develpoment

---
+++ src/app/crc/tab.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'tab',
+  templateUrl: './tab.html',
+  styleUrls:['./tab.scss']
+})
+export class TabComponent {
+  public profile: any[] = [
+    { img: 'attached-file.png', probar: '100%', probarOK: 'progressOk' },
+    { img: 'attached-file.png', probar: '30%', probarOK: 'progressinComp' },
+    { img: 'attached-file.png', probar: '30%', probarOK: 'progressinComp' },
+    { img: 'attached-file.png', probar: '30%', probarOK: 'progressinComp' }
+  ]
+  public media: any[] = [
+    { img: 'medical-info.png', probar: '100%', probarOK: 'progressOk' },
+    { img: 'medical-info.png', probar: '30%', probarOK: 'progressinComp' },
+    { img: 'medical-info.png', probar: '30%', probarOK: 'progressinComp' },
+    { img: 'medical-info.png', probar: '30%', probarOK: 'progressinComp' }
+  ]
+  public tags: any[] = [
+    { fName: 'backup_3.zip', tag: ['identifying', 'Authenticating', 'unAuthenticating'] },
+    { fName: 'backup_3.zip', tag: ['identifying', 'unAuthenticating'] },
+    { fName: 'backup_3.zip', tag: ['identifying', 'Authenticating', 'unAuthenticating'] }
+  ]
+}
